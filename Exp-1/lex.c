ce5eeccba1a4eae6d0f437c97f1790aa5067e70b
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-04 12:06:08 +0530

    Exp 1 started

---
+++ Exp-1/lex.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+void main(){
+    char operators[]="+-/*=<>";
+    char buffer[100];
+    char keywords[10][10]={"if","else","printf","while","int","char"};
+
+    
+}
