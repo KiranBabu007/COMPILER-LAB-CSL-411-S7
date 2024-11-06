34cc5bfe5bdd4f7fcd86993159e58431d5ae1918
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-07 00:45:15 +0530

    Add lexer implementation and improve file handling in lex.c

--- Exp-1/lex.c
+++ Exp-1/lex.c
@@ -1,70 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-
-void main(){
-    char operators[]="+-/*=<>";
+void main() {
+    char operators[] = "+-/*=<>";
     char buffer[100];
-    char keywords[10][10]={"if","else","printf","while","int","char"};
-
-    int bindex=0;
-
+    char keywords[10][10] = { "if", "else", "printf", "while", "int", "char" };
+    char c;
 
     FILE *f;
 
     f=fopen("lex.txt","r");
-    fgets(buffer,sizeof(buffer),f);
-    printf("Content of the file:%s\n",buffer);
-    bzero(buffer,100);
-    fclsoe(f);
-
-    f=fopen("lex.txt","r");
-    printf("Output:");
-
-    while(1){
-     char c=getc(f);
-     if(feof(f))
-     break;
-
-     if(isalnum(c)){
-        buffer[bindex++]=c;
-        while(isalnum(c=fgetc(f))){
-            buffer[bindex++]=c;
-        }
-     }
-     buffer[bindex]="\0";
-     ungetc(c,f);
-    int is_key=0;
-     for(int i=0;i<10;i++){
-        if(strcmp(buffer,keywords[i]==0)){
-            printf("%s is a keyword\n",buffer);
-            is_key=1;
-            break;
-        }
-     }
-
-        if(!is_key){
-            printf("%s is an identifer\n",buffer);
-        }
-        bindex=0;
-     
-
-     if(strchr(operators,c)){
-        printf("%c is an operator\n",c);
-     }
-     else if(isspace(c)){
-        continue;
-     }
-     else{
-        printf("%c is a special symbol");}
-    }
-     }
-
-
-
-
-
     
 
+    if (!f){
+        printf("File not found");
+        return ;
+    }
+
+    while((c=fgetc(f))!=EOF){
+        if(isalnum(c)){
+            int i=0;
+            while(isalnum(c)){
+                buffer[i++]=c;
+                c=fgetc(f);
+            }
+            buffer[i]='\0';
+            ungetc(c,f);
+
+        int iskey=0;
+        for(int j=0;j<10;j++){
+            if(strcmp(buffer,keywords[j])==0){
+                printf("%s is a keyword\n",buffer);
+                iskey=1;
+            }
+        }
+        if(!iskey){
+            printf("%s is an identifier\n",buffer);
+            memset(buffer,0,100);
+        }
+    }
+    else if(strchr(operators,c)){
+        printf("%c is an operator\n",c);
+    }
+    else if(isspace(c)){
+        continue;
+    }
+    else{
+        printf("%c is a special character\n",c);
+    }
+    }
+
+}
